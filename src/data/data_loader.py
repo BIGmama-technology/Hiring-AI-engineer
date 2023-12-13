d002e4390eae6abdaf9b0f07c3de24d02c4798e9
AmineMAALEM <aminemaalempro@gmail.com>
2023-12-13 01:23:25 +0100

    deleting __init__.py we dont need it for python 3.3+

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -14,62 +14,38 @@ def normalize(x: Union[pd.Series, np.ndarray]) -> Union[pd.Series, np.ndarray]:
     return ((x - x.mean()) / x.std() )
 
 
-def load_mauna_loa_atmospheric_co2(file_path):
-    # Load data into a DataFrame
-    df = pd.read_csv(file_path)
-    # Prepare the data
-    X = df[["decimal date"]].values.reshape(-1, 1)
-    y = df["average"].values
-
-    # Normalize the data for numerical stability
-    X_normalized = normalize(X)
-    return X, y, X_normalized
-
-
-def load_international_airline_passengers(file_path):
-    # Load  data into a DataFrame
-    df_airpassengers = pd.read_csv(file_path)
-
-    # Prepare the data
-    X_airpassengers = (
-        pd.to_datetime(df_airpassengers["Month"])
-        .dt.to_period("M")
-        .astype("int64")
-        .values.reshape(-1, 1)
-    )
-    y_airpassengers = df_airpassengers["Passengers"].values
-
-    # Normalize the data for numerical stability
-    X_airpassengers_normalized = normalize(X_airpassengers)
-
-    return X_airpassengers, y_airpassengers, X_airpassengers_normalized
-
-
-def load_data_set(file_path, feature_column, target_column, normalized = True):
+def load_data_set(file_path : str, feature_column : str, target_column : str, normalized : bool=True, convert_to_int : bool=False) -> tuple[Union[np.ndarray, pd.Series], np.ndarray, Union[np.ndarray, pd.Series]]:
     """
-    Load any dataset from a CSV file format
+        Load any dataset from a CSV file format
 
-    Args:
-        file_path (str): The path to the CSV file
-        feature_column(str): The name of the feature column
-        target_column(str): The name of the target column
-        normalized (bool): Whether to normalize the data or not
+        Args:
+            file_path (str): The path to the CSV file
+            feature_column(str): The name of the feature column
+            target_column(str): The name of the target column
+            normalized (bool): Whether to normalize the data or not
+            convert_to_int (bool): Whether to convert the feature to int64
 
-    Returns:
-        Tuple: a tuple containing X, y, and optionally X_normalized
+        Returns:
+            Tuple: a tuple containing X, y, and optionally X_normalized
     """
     # Load data into a DataFrame
     df = pd.read_csv(file_path)
 
+    # Handle different data types for the feature column
+    if df[feature_column].dtype == 'object':
+        # convert to datetime
+        df[feature_column] = pd.to_datetime(df[feature_column], errors='coerce')
+    if convert_to_int:
+        # convert the feature to int64
+        df[feature_column] = df[feature_column].astype('int64')
+
     # Prepare the data
-    #X = df[feature_column].values.reshape(-1, 1)
-    X = pd.to_datetime(df[feature_column]).astype("int64").values.reshape(-1, 1)
+    X = df[feature_column].values.reshape(-1, 1)
     y = df[target_column].values
-    # Normalize the data for numerical stability
+
+    # Normalize the data for numerical stability if specified
     if normalized:
         X_normalized = normalize(X)
         return X, y, X_normalized
     else:
         return X, y
-
-
