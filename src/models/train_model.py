2eefe853d5c3ea4444a74125f8f09b3a3caccc5a
miro-4231 <131700226+miro-4231@users.noreply.github.com>
2023-12-16 18:50:05 +0100

    Early Stopping
    added early stopping criterion to the train loop, which enhanced the scores of the models in the test set

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -39,6 +39,7 @@ def train_test_model(model_class, dataset, m, layer_dim: list):
     X1_train_tensor = torch.from_numpy(X1_train).float()
     y1_train_tensor = torch.from_numpy(y1_train).float()
     X1_test_tensor = torch.from_numpy(X1_test).float()
+    y1_test_tensor = torch.from_numpy(y1_test).float()
 
     # Define the Bayesian neural network model
     input_size = X1_train.shape[1]
@@ -51,7 +52,11 @@ def train_test_model(model_class, dataset, m, layer_dim: list):
     # Training loop
     num_epochs = 1000
     train_losses = []
-
+    argmin = num_epochs + 1
+    t = 0
+    test_losses = []
+    argmin_test = num_epochs + 1
+    t_test = 0
     with tqdm(range(num_epochs), unit="epochs") as pbar:
         for epoch in pbar:
             pbar.set_description(f"training {epoch+1}")
@@ -66,8 +71,29 @@ def train_test_model(model_class, dataset, m, layer_dim: list):
 
             train_losses.append(loss.item())
             pbar.set_postfix(loss=train_losses[-1], min_loss=min(train_losses))
+            with torch.no_grad():
+                model.eval()
+                predictions_1 = model(X1_test_tensor)
+                loss = loss_function(predictions_1, y1_test_tensor)
+                test_losses.append(loss.item())
+
+            # early stopping
+            t += 1
+            if argmin != np.argmin(train_losses):
+                argmin = np.argmin(train_losses)
+                t = 0
+            if t > 20:
+                break
+
+            t_test += 1
+            if argmin_test != np.argmin(test_losses):
+                argmin_test = np.argmin(test_losses)
+                t_test = 0
+            if t_test > 15:
+                break
+
     # ---------  Plot training losses  ----------
-    plt.plot(range(1, num_epochs + 1), train_losses, label="Training Loss")
+    plt.plot(range(1, epoch + 2), train_losses, label="Training Loss")
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
     plt.title("Training Loss Over Epochs")
@@ -88,12 +114,11 @@ def train_test_model(model_class, dataset, m, layer_dim: list):
     with torch.no_grad():
         model.eval()
         predictions_1 = model(X1_test_tensor)
-        print(
-            f"test score:{loss_function(predictions_1, torch.from_numpy(mean.unsqueeze()).float()).item()}"
-        )
-    # # Convert predictions to NumPy array for plotting
-    # predictions_np_1 = predictions_1.numpy()
 
+    # Convert predictions to NumPy array for plotting
+    predictions_np_1 = predictions_1.numpy()
+    print(f"test score eval:{np.square(np.subtract(predictions_np_1,y1_test)).mean()}")
+    print(f"test score mean:{np.square(np.subtract(mean,y1_test)).mean()}")
     # export model
     torch.save(model, "./models/" + dataset + "_model.pth")
     # print(X1_test.shape)
