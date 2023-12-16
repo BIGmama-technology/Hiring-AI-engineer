67789004674fdc5bffcba35f52136ac06bf8f5eb
miro-4231 <131700226+miro-4231@users.noreply.github.com>
2023-12-16 17:31:58 +0100

    Create SimpleModel.py
    a custom class to create an MLP of different sizes, it contains batch normalization and dropout layers

---
+++ src/models/SimpleModel.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+class Mlp(nn.Module):
+    def __init__(self, *args):
+        super().__init__()
+        if len(args) < 3:
+            raise ValueError("at least need two values to create an mlp")
+        else:
+            num_features = args
+
+        self.layers = []
+        for i in range(len(num_features) - 1):
+            if i < (len(num_features) - 2):
+                layer = nn.Sequential(
+                    nn.Linear(num_features[i], num_features[i + 1]),
+                    nn.BatchNorm1d(num_features[i + 1]),
+                    nn.ReLU(),
+                    nn.Dropout(),  # (1 - 10/num_features[i+1]))
+                )
+            else:
+                layer = nn.Linear(num_features[i], num_features[i + 1])
+
+            self.layers.append(layer)
+        self.layers = nn.ParameterList(self.layers)
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
