b3fb8115d52190df1fb09513fda8c93ed1a44f06
Abdelkrim Nachef <abdelkrim.nachef@icosnet.com>
2023-12-31 15:09:19 +0100

    clean  the code

--- src/models/train_bnn.py
+++ src/models/train_bnn.py
@@ -10,9 +10,6 @@ from src.data.data_loader import (
     load_international_airline_passengers,
 )
 
-MUANA_DATA_PATH = "data/mauna_loa_atmospheric_co2.csv"
-AIRLINE_DATA_PATH = "data/international-airline-passengers.csv"
-
 
 def train_model(X_train, y_train, model, optimizer, loss_function, num_epochs):
     train_losses = []
@@ -25,6 +22,18 @@ def train_model(X_train, y_train, model, optimizer, loss_function, num_epochs):
         optimizer.step()
         train_losses.append(loss.item())
     return train_losses
+
+
+def train_model_variational(X_train, y_train, model, optimizer, num_epochs):
+    train_losses = []
+    for epoch in tqdm(range(num_epochs), desc="Training", unit="epoch"):
+        model.train()
+        optimizer.zero_grad()
+        loss = model.elbo_loss(X_train, y_train)
+        loss.backward()
+        optimizer.step()
+        train_losses.append(loss.item())
+    return model, train_losses
 
 
 def plot_training_loss(train_losses, title="Training Loss Over Epochs"):
@@ -47,126 +56,98 @@ def plot_predictions(X_test, y_test, predictions, title="True Values vs Predicti
     plt.show()
 
 
-# ------------------------------------------
-# mauna_loa_atmospheric_co2 Dataset
-# ------------------------------------------
+def run_training(
+    X_train, y_train, X_test, y_test, model, optimizer, loss_function, num_epochs
+):
+    train_losses = train_model(
+        X_train, y_train, model, optimizer, loss_function, num_epochs
+    )
+    plot_training_loss(train_losses)
 
+    with torch.no_grad():
+        model.eval()
+        predictions = model(X_test)
+        predictions_np = predictions.numpy()
+
+    plot_predictions(X_test, y_test, predictions_np)
+    return model
+
+
+MUANA_DATA_PATH = "data/mauna_loa_atmospheric_co2.csv"
+AIRLINE_DATA_PATH = "data/international-airline-passengers.csv"
+
+# mauna_loa_atmospheric_co2 Dataset
 X1, y1, X1_normalized = load_mauna_loa_atmospheric_co2(MUANA_DATA_PATH)
 X1_train, X1_test, y1_train, y1_test = train_test_split(
     X1_normalized, y1, test_size=0.2, random_state=42
 )
-
 X1_train_tensor = torch.from_numpy(X1_train).float()
 y1_train_tensor = torch.from_numpy(y1_train).float()
 X1_test_tensor = torch.from_numpy(X1_test).float()
 
-input_size = X1_train.shape[1]
-hidden_size = 20
-output_size = 1
-model = BayesianModel(input_size, hidden_size, output_size, 5)
-
-loss_function = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-num_epochs = 1000
-
-train_losses = train_model(
-    X1_train_tensor, y1_train_tensor, model, optimizer, loss_function, num_epochs
-)
-plot_training_loss(train_losses)
-
-with torch.no_grad():
-    model.eval()
-    predictions_1 = model(X1_test_tensor)
-    predictions_np_1 = predictions_1.numpy()
-
-plot_predictions(X1_test, y1_test, predictions_np_1)
-
-torch.save(model, "./models/mauna_loa_model.pth")
-
-# ------------------------------------------
 # international-airline-passengers Dataset
-# ------------------------------------------
-
 X2, y2, X2_normalized = load_international_airline_passengers(AIRLINE_DATA_PATH)
 X2_train, X2_test, y2_train, y2_test = train_test_split(
     X2_normalized, y2, test_size=0.2, random_state=42
 )
-
 X2_train_tensor = torch.from_numpy(X2_train).float()
 y2_train_tensor = torch.from_numpy(y2_train).float()
 X2_test_tensor = torch.from_numpy(X2_test).float()
 
-input_size = X2_train.shape[1]
-model = BayesianModel(input_size, hidden_size, output_size, num_layers=5)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-
-train_losses = train_model(
-    X2_train_tensor, y2_train_tensor, model, optimizer, loss_function, num_epochs
-)
-plot_training_loss(train_losses)
-
-with torch.no_grad():
-    model.eval()
-    predictions_2 = model(X2_test_tensor)
-    predictions_np_2 = predictions_2.numpy()
-
-plot_predictions(X2_test, y2_test, predictions_np_2)
-
-torch.save(model, "./models/international_airline_passengers_model.pth")
-
-############################## variational #######################
-# dataset 1
-def train_model_variational(X_train, y_train, model, optimizer, num_epochs):
-    train_losses = []
-    for epoch in tqdm(range(num_epochs), desc="Training", unit="epoch"):
-        model.train()
-        optimizer.zero_grad()
-        loss = model.elbo_loss(X_train, y_train)
-        loss.backward()
-        optimizer.step()
-        train_losses.append(loss.item())
-    return train_losses
-
-
+# Common model parameters
 input_size = X1_train.shape[1]
 hidden_size = 20
 output_size = 1
-model = BayesianModel(input_size, hidden_size, output_size, num_layers=5)
-
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 num_epochs = 1000
 
-train_losses = train_model_variational(
-    X1_train_tensor, y1_train_tensor, model, optimizer, num_epochs
+# Training and evaluating models for mauna_loa_atmospheric_co2 Dataset
+model_1 = BayesianModel(input_size, hidden_size, output_size, 5)
+optimizer_1 = torch.optim.Adam(model_1.parameters(), lr=0.01)
+loss_function = nn.MSELoss()
+
+model_1 = run_training(
+    X1_train_tensor,
+    y1_train_tensor,
+    X1_test_tensor,
+    y1_test,
+    model_1,
+    optimizer_1,
+    loss_function,
+    num_epochs,
 )
-plot_training_loss(train_losses)
+torch.save(model_1.state_dict(), "./models/mauna_loa_model.pth")
 
-with torch.no_grad():
-    model.eval()
-    predictions_1 = model(X1_test_tensor)
-    predictions_np_1 = predictions_1.numpy()
+# Training and evaluating models for international-airline-passengers Dataset
+model_2 = BayesianModel(input_size, hidden_size, output_size, 5)
+optimizer_2 = torch.optim.Adam(model_2.parameters(), lr=0.01)
 
-plot_predictions(X1_test, y1_test, predictions_np_1)
-
-torch.save(model.state_dict(), "./models/mauna_loa_model.pth")
-
-# dataset 2
-input_size = X2_train.shape[1]
-model = BayesianModel(input_size, hidden_size, output_size, num_layers=5)
-
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-num_epochs = 1000
-
-train_losses = train_model_variational(
-    X2_train_tensor, y2_train_tensor, model, optimizer, num_epochs
+model_2 = run_training(
+    X2_train_tensor,
+    y2_train_tensor,
+    X2_test_tensor,
+    y2_test,
+    model_2,
+    optimizer_2,
+    loss_function,
+    num_epochs,
 )
-plot_training_loss(train_losses)
+torch.save(model_2.state_dict(), "./models/international_airline_passengers_model.pth")
 
-with torch.no_grad():
-    model.eval()
-    predictions_2 = model(X2_test_tensor)
-    predictions_np_2 = predictions_2.numpy()
+# Training using variational inference for both datasets
+model_1_var = BayesianModel(input_size, hidden_size, output_size, 5)
+optimizer_1_var = torch.optim.Adam(model_1_var.parameters(), lr=0.01)
+model_1_var, losses_1_var = train_model_variational(
+    X1_train_tensor, y1_train_tensor, model_1_var, optimizer_1_var, num_epochs
+)
+plot_training_loss(losses_1_var)
+torch.save(model_1_var.state_dict(), "./models/mauna_loa_model_var.pth")
 
-plot_predictions(X2_test, y2_test, predictions_np_2)
-
-torch.save(model.state_dict(), "./models/international_airline_passengers_model.pth")
+model_2_var = BayesianModel(input_size, hidden_size, output_size, 5)
+optimizer_2_var = torch.optim.Adam(model_2_var.parameters(), lr=0.01)
+model_2_var, losses_2_var = train_model_variational(
+    X2_train_tensor, y2_train_tensor, model_2_var, optimizer_2_var, num_epochs
+)
+plot_training_loss(losses_2_var)
+torch.save(
+    model_2_var.state_dict(), "./models/international_airline_passengers_model_var.pth"
+)
